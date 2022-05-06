f12ed5915c2f23c8d6a66adfbfe23055ee9ebf69
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-06 23:01:31 +0100

    set up routes, api and controllers

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const {router} = require('express');
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Wrong route!'));
+
+module.exports = router;
