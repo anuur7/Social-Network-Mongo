b49bf476ee2204e31af8d342e740979e54a36950
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-07 13:44:38 +0100

    connected to server

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,8 @@
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/mySocial", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,29 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const db = require('./config/connection');
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/connection");
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
-const init = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("[INFO]: Database connection successful");
-
-    app.listen(PORT, () => 
-    console.log(`Server running on http://localhost:${PORT}`));
-  } catch (err) {
-    console.log(`[ERROR]: Database connection failed | ${err.message}`)
-  }
-}
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
